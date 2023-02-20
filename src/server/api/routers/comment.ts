be087dcb6d14b2d50d0fc79314a56d3a8b05d8b4
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-02-20 19:31:16 +0100

    comments query procedure

--- src/server/api/routers/comment.ts
+++ src/server/api/routers/comment.ts
@@ -29,4 +29,38 @@ export const commentRouter = createTRPCRouter({
         },
       });
     }),
+
+  comment: protectedProcedure
+    .input(
+      z.object({
+        postId: z.string(),
+        cursor: z.string().nullish(),
+        limit: z.number().min(1).max(100).default(10),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { cursor, limit, postId } = input;
+      const { prisma } = ctx;
+
+      const comments = await prisma.comment.findMany({
+        take: limit + 1,
+        where: {
+          postId,
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+
+      if (comments.length > limit) {
+        const nextItem = comments.pop() as (typeof comments)[number];
+
+        nextCursor = nextItem.id;
+      }
+
+      return {
+        comments,
+        nextCursor,
+      };
+    }),
 });
