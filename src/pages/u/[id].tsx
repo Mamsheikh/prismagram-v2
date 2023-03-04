a5caf8440243d538baa276ce8df51d844d63206b
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-03-04 17:24:10 +0100

    profile page mobile for session user

--- src/pages/u/[id].tsx
+++ src/pages/u/[id].tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { BiGridVertical } from "react-icons/bi";
-import { MdOutlineContactMail } from "react-icons/md";
+import { CgDisplayFullwidth } from "react-icons/cg";
 
 import toast from "react-hot-toast";
 
@@ -12,13 +12,14 @@ import Layout from "../../components/Layout";
 import { IoSettingsOutline } from "react-icons/io5";
 import Head from "next/head";
 import { BsBookmark } from "react-icons/bs";
-import { AiOutlinePlayCircle } from "react-icons/ai";
+import { IoMdGrid } from "react-icons/io";
 import { useSession } from "next-auth/react";
 import { api } from "../../utils/api";
 import FollowBtn from "../../components/common/FollowBtn";
 import { useQueryClient } from "@tanstack/react-query";
 import FollowersModal from "../../components/User/FollowersModal";
 import FollowingModal from "../../components/User/FollowingModal";
+import { Tab } from "@headlessui/react";
 
 const Profile: React.FC = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -48,6 +49,10 @@ const Profile: React.FC = (props) => {
   if (!user || isLoading) {
     return <div>Loading user profile....</div>;
   }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function classNames(...classes: any) {
+    return classes.filter(Boolean).join(" ");
+  }
   const isFollowing = user?.isFollowing;
   return (
     <Layout>
@@ -57,34 +62,102 @@ const Profile: React.FC = (props) => {
         </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="mx-auto max-w-6xl justify-center overflow-y-auto p-10 pt-20 scrollbar scrollbar-thumb-black dark:text-white xl:mx-auto">
-        <div className="grid grid-cols-4 gap-4">
-          <div className="col-span-1 flex justify-center">
+      <div className="justify-center overflow-y-auto  pt-20 scrollbar scrollbar-thumb-black dark:text-white xl:mx-auto">
+        <div className="">
+          <div className="flex space-x-6 px-7">
             <Image
               height={36}
               width={36}
-              className="h-36 w-36 rounded-full object-cover"
+              className="h-[77px] w-[77px] flex-shrink-0 rounded-full object-cover"
               src={user.image as string}
               alt=""
             />
-          </div>
-          <div className="col-span-3">
-            <span className="mr-4 text-2xl text-gray-600 dark:text-white">
-              {user.username}
-            </span>
-            {session?.user?.id === user.id ? (
-              <>
-                <div className="mr-4 inline cursor-pointer rounded border border-gray-300 p-1 px-2 text-sm text-gray-700 dark:text-white">
-                  <button onClick={() => console.log("hello")}>
+            <div className="flex flex-1 flex-col">
+              {session?.user?.id === user.id ? (
+                <>
+                  <span className=" text-2xl text-gray-600 dark:text-white">
+                    {user.username}
+                    <IoSettingsOutline className="ml-4 inline h-6 flex-1 cursor-pointer" />
+                  </span>
+                  <button
+                    onClick={() => console.log("hello")}
+                    className="mt-2 w-full rounded-md bg-gray-300 px-3 py-1 font-semibold"
+                  >
                     Edit Profile
                   </button>
-                </div>
-                {/* {isOpen && <EditProfileModal user={user} />} */}
-                <IoSettingsOutline className="inline h-6 flex-1 cursor-pointer" />
-              </>
-            ) : (
-              <FollowBtn isFollowing={isFollowing} userId={user.id} />
-            )}
+                </>
+              ) : null}
+            </div>
+          </div>
+          <div className="mt-4 mb-3 border-b border-gray-300 px-7 pb-6">
+            <span className="font-semibold">{user.name}</span>
+          </div>
+          <div className="flex items-center justify-between space-x-3 border-b border-gray-300 px-9 pb-4">
+            <div className="flex flex-col items-center justify-center">
+              <span className="font-semibold">{user._count.posts}</span>
+              <span className="text-sm text-gray-400">posts</span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <span className="font-semibold">{user._count.followers}</span>
+              <span className="text-sm text-gray-400">followers</span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <span className="font-semibold">{user._count.following}</span>
+              <span className="text-sm text-gray-400">following</span>
+            </div>
+          </div>
+          <div className="w-full">
+            <Tab.Group>
+              <Tab.List className="flex space-x-1 border-b border-gray-300 pb-1">
+                <Tab
+                  className={({ selected }) =>
+                    classNames(
+                      "flex w-full items-center justify-center  py-2.5 text-sm font-medium leading-5 text-gray-700",
+                      " focus:outline-none ",
+                      selected
+                        ? "text-blue-500"
+                        : "text-gray-500 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  <IoMdGrid className="h-6 w-6" />
+                </Tab>
+                <Tab
+                  className={({ selected }) =>
+                    classNames(
+                      "flex w-full items-center justify-center rounded-lg py-2.5 text-sm font-medium leading-5 text-gray-700",
+                      " focus:outline-none ",
+                      selected
+                        ? "text-blue-500"
+                        : "text-gray-500 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  <CgDisplayFullwidth className="h-6 w-6" />
+                </Tab>
+                <Tab
+                  className={({ selected }) =>
+                    classNames(
+                      "flex w-full items-center justify-center  py-2.5 text-sm font-medium leading-5 text-gray-700",
+                      " focus:outline-none ",
+                      selected
+                        ? "text-blue-500"
+                        : "text-gray-500 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  <BsBookmark className="h-6 w-6" />
+                </Tab>
+              </Tab.List>
+              <Tab.Panels>
+                <Tab.Panel>Content 1</Tab.Panel>
+                <Tab.Panel>Content 2</Tab.Panel>
+                <Tab.Panel>Content 3</Tab.Panel>
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
+
+          {/* <div className="">
             <div className="mt-4 flex">
               <div>
                 <span className="font-semibold">{user._count.posts}</span> posts
@@ -111,7 +184,7 @@ const Profile: React.FC = (props) => {
             <div>
               <div className="flex flex-col  pt-3">
                 <span className="text-lg font-bold text-gray-700 dark:text-gray-400">
-                  {/* {user.bio} */}
+                  {/* {user.bio} 
                   this is my bio
                 </span>
 
@@ -121,12 +194,12 @@ const Profile: React.FC = (props) => {
                   //   href={user.website}
                   className="mr-2 text-base font-medium text-blue-700"
                 >
-                  {/* {user.website} */}
+                  {/* {user.website} 
                   mamsheikh.vercel.app
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
         <FollowersModal
           isOpen={isOpen}
@@ -140,9 +213,9 @@ const Profile: React.FC = (props) => {
         />
         {/* {showFollowers && <Followers users={user.followers} />}
         {showFollowing && <Following users={user.following} />} */}
-        <hr className="mt-6 border-gray-500" />
+        {/* <hr className="mt-6 border-gray-500" /> */}
 
-        <div className="flex justify-center gap-10">
+        {/* <div className="flex justify-center gap-10">
           <button className="flex gap-1 border-gray-800 py-4 text-sm font-semibold uppercase text-gray-400 focus:border-t focus:text-gray-600">
             <BiGridVertical className="h-5 w-5 items-center" /> POSTS
           </button>
@@ -155,7 +228,7 @@ const Profile: React.FC = (props) => {
           <button className="flex gap-1 border-gray-800 py-4 text-sm font-semibold uppercase text-gray-400 focus:border-t focus:text-gray-600">
             <MdOutlineContactMail className="h-5 w-5 items-center" /> Tagged
           </button>
-        </div>
+        </div> */}
         <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
           {/* {posts.map((post) => (
             <div key={post.id} className='group relative cursor-pointer'>
