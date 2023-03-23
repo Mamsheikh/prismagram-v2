3826b97f304fac22d3a401ccb7864d076af00264
Munir Ali Mohammed <mamsheikh123@gmail.com>
2023-03-23 21:05:53 +0100

    added nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
-import { type AppType } from "next/app";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import { type AppType } from "next/app";
+import NextNprogress from "nextjs-progressbar";
 import { Toaster } from "react-hot-toast";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import "nprogress/nprogress.css";
 
 import { api } from "../utils/api";
 
@@ -16,6 +19,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
+        <NextNprogress
+          color="#0079bf"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={4}
+        />
         <Component {...pageProps} />
         <Toaster />
         <ReactQueryDevtools initialIsOpen={false} />
